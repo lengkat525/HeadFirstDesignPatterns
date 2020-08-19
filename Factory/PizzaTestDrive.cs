cb1a8a3c3c2e9456939a567c2204496d98ebaed3
Katrina Leng <lengkat@qq.com>
2020-08-19 10:36:23 +0800

    move file

--- Factory/PizzaTestDrive.cs
+++ Factory/PizzaTestDrive.cs
@@ -1,17 +1,22 @@
 ﻿using System;
+using Factory.pizzas;
 
-namespace Factory.pizzas
+namespace Factory
 {
     class PizzaTestDrive
     {
         public static void Main(string[] args)
         {
+            //pizzas
             SimplePizzaFactory factory = new SimplePizzaFactory();
             PizzaStore store = new PizzaStore(factory);
 
             Pizza pizza = store.OrderPizza("cheese");
             Console.WriteLine("We ordered a {0} \n{1}", pizza.GetName(), pizza);
             Console.Read();
+
+            //pizzafm
+
         }
     }
 }
